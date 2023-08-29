0ffc34487671ab715992815b70dc99e64f07fb2c
nurlanaydinov <nurlanaydinov@gmail.com>
2023-08-29 20:39:33 +0400

    create_user_form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { DashboardComponent } from './admin/components/dashboard/dashboard.compo
 import { HomeComponent } from './ui/components/home/home.component';
 
 const routes: Routes = [
-  { 
+  {
     path: "admin", component: LayoutComponent, children: [
       { path: "", component: DashboardComponent },
-      { path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule)},
-      { path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule)},
-      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule)}
+      { path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule) },
+      { path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule) },
+      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) }
     ]
   },
-  { path: "", component: HomeComponent},
-  { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule)},
-  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule)}
+  { path: "", component: HomeComponent },
+  { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
+  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
+  { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule) },
+  { path: "login", loadChildren: () => import("./ui/components/login/login.module").then(module => module.LoginModule) }
 ];
 
 @NgModule({
